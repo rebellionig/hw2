dd7f323b358d7ebbc8dffb3fa695613167bb7f83
rebellionig <salkenarman82@gmail.com>
2024-09-22 21:05:28 +0500

    init

---
+++ homework3.java
@@ -0,0 +1,78 @@
+package shadenade62.javahomework.lec3;
+
+import java.util.Scanner;
+
+public class homework3
+{
+    public static void main(String[]args)
+    {
+        greetings();
+        int res = checkSign();
+        if (res>=0)
+        {
+            System.out.println("the sum is positive");
+        } else
+        {
+            System.out.println("The sum is negative");
+        }
+        selectColor();
+        CompareNumbers();
+        int initValue=10;
+        int delta=5;
+        boolean increment=true;
+        addOrSubtractandPrint(initValue,delta,increment);
+    }
+
+    public static void greetings(){
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+    public static int checkSign()
+    {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        int c=scanner.nextInt();
+        return a+b+c;
+    }
+    public static void selectColor() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer:");
+        int data = scanner.nextInt();
+        if (data <= 10) {
+            System.out.println("Red");
+        } else if (data <= 20) {
+            System.out.println("yellow");
+        } else {
+            System.out.println("green");
+        }
+    }
+
+    public static void CompareNumbers()
+    {
+        Scanner scanner=new Scanner(System.in);
+        int a=scanner.nextInt();
+        int b=scanner.nextInt();
+        if(a>=b)
+        {
+            System.out.println("a>=b");
+        }
+        else
+        {
+            System.out.println("a<b");
+        }
+    }
+    public static void addOrSubtractandPrint(int initValue,int delta, boolean increment)
+    {
+        if(increment)
+        {
+            System.out.println(initValue+delta);
+        }
+        else
+        {
+            System.out.println(initValue-delta);
+        }
+    }
+}
